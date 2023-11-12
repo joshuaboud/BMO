c2ebfaccbdf0a5596aa94d89f9b801bee9543298
Josh Boudreau <joshuaboud@hotmail.com>
2023-11-12 02:12:38 -0400

    save wav

--- tts/tts-test-client.py
+++ tts/tts-test-client.py
@@ -4,6 +4,8 @@ import argparse
 import sounddevice as sd
 import requests
 import numpy as np
+import soundfile as sf
+from typing import Tuple
 
 
 def parse_content_type(content_type: str) -> dict:
@@ -17,13 +19,18 @@ def parse_content_type(content_type: str) -> dict:
     raise ValueError(f'Invalid content type: {content_type}')
 
 
-def say(text: str, host: str, port: int) -> None:
+def get_pcm(text: str, host: str, port: int) -> Tuple[np.ndarray, dict]:
     print("Requesting speech synthesis...")
     r = requests.post(f'http://{host}:{port}/api/v1/synthesize', json={'text': text})
     r.raise_for_status()
     audio_info = parse_content_type(r.headers['Content-Type'])
     print(audio_info)
     pcm = np.frombuffer(r.content, dtype=audio_info['dtype'])
+    return pcm, audio_info
+
+
+def say(text: str, host: str, port: int) -> None:
+    pcm, audio_info = get_pcm(text, host, port)
     print("Playing audio...")
     audio_stream = sd.OutputStream(samplerate=audio_info['samplerate'], channels=1, blocksize=2048, dtype=audio_info['dtype'])
     audio_stream.start()
@@ -31,13 +38,23 @@ def say(text: str, host: str, port: int) -> None:
     audio_stream.stop()
 
 
+def save_wav(text: str, host: str, port: int, file_path: str) -> None:
+    pcm, audio_info = get_pcm(text, host, port)
+    with sf.SoundFile(file_path, mode='w', samplerate=audio_info['samplerate'], channels=1, subtype='FLOAT', format='WAV') as wv:
+        wv.write(pcm)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-H', '--host', type=str, required=False, default='127.0.0.1', metavar='HOST', help="Server host")
     parser.add_argument('-p', '--port', type=int, required=True, metavar='PORT', help="Server port number")
+    parser.add_argument('-w', '--output-wav', type=str, required=False, default=None, dest='output_wav_path', metavar='PATH.wav', help="Save output to .wav instead of playing")
     parser.add_argument('text', type=str, action='append', metavar='TEXT', help="Text to send")
     args = parser.parse_args()
-    say(' '.join(args.text), args.host, args.port)
+    input_text = ' '.join(args.text)
+    if args.output_wav_path is not None:
+        return save_wav(input_text, args.host, args.port, args.output_wav_path)
+    say(input_text, args.host, args.port)
 
 if __name__ == "__main__":
     main()
